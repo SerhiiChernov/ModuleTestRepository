2bfca245155319a7e3102a928e3b5cc72357d70d
Serhii-Chernov <chernovsrg@gmail.com>
2024-04-05 12:35:15 +0300

    added function write to file freaquency and changed the sample.txt

--- main.py
+++ main.py
@@ -16,3 +16,20 @@ def find_words_frequency(text, num_words=10):
     word_freq = Counter(words)
     return word_freq.most_common(num_words)
 
+def write_to_file(most_common_words, output_file_path):
+    """Записує найпопулярніші слова у файл."""
+    with open(output_file_path, 'w', encoding='utf-8') as file:
+        for word, count in most_common_words:
+            file.write(f"{word}-{count}\n")
+
+if __name__ == "__main__":
+    file_path = 'sample.txt'  # Шлях до вашого файлу
+    output_file_path = 'most_common_words.txt'  # Ім'я вихідного файлу
+    num_words = 10  # Кількість найпопулярніших слів
+
+    text = read_file(file_path)
+    cleaned_text = clean_text(text)
+    most_common_words = find_words_frequency(cleaned_text, num_words)
+    write_to_file(most_common_words, output_file_path)
+
+    print(f"Знайдено {num_words} найпопулярніших слів. Результат збережено у файлі {output_file_path}.")
