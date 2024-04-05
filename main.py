ba7685f7487d0e4908a3ab9d91cacfc39fb3e45a
Serhii-Chernov <chernovsrg@gmail.com>
2024-04-05 12:26:49 +0300

    added function to clean the text and added the sample.txt

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ def read_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read().lower()
 
+def clean_text(text):
+    """Видаляє зайві символи з тексту."""
+    return re.sub(r'[^a-zA-Zа-яА-ЯіІїЇєЄёЁ\s]', '', text)
+
 
